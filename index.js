5b816147a5920b5df24c1cbb786929bff2cc520d
Habib Ur Rehman <Hbrehman@codingStar.github.com>
2020-03-22 13:07:18 +0500

    Implemented tours overview page with pug and rendered tours data

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const rateLimit = require('express-rate-limit');
 const express = require('express');
 const morgan = require('morgan');
@@ -5,11 +6,20 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
+const viewRouter = require('./routes/viewRoutes');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) Global Middleware
+// Middleware to serve static content
+app.use(express.static(path.join(__dirname, 'public')));
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -22,24 +32,18 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.static(`${__dirname}/public`));
-
 console.log(process.env.NODE_ENV);
 
 app.use(express.json());
 
 // This process is called mounting
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-
-  // const err = new Error();
-  // err.status = 'fail';
-  // err.statusCode = 404;
-  // next(err);
 });
 
 app.use(globalErrorHandler);
